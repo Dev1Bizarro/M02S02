9b79bf58894022e4253323fa33d7447df5f710ec
Gustavo Bizarro Lombardo <90002024+Dev1Bizarro@users.noreply.github.com>
2023-01-29 15:40:51 -0300

    Delete script1.js

--- script1.js
+++
@@ -1,9 +0,0 @@
-var nome = window.prompt('digite seu nome:');
-
-
-function passagem(nome) {
-    var mensagem = `Sr ${nome}, está confirmado a sua viagem.`;
-
-    document.write(`<h1> ${mensagem} </h1>`);
-}
-passagem(nome);
